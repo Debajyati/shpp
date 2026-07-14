32d7f454ca76506b896eba2fa3a0dddfe98f0499
Debajyati Dey <ddebajyati@gmail.com>
2026-07-14 19:28:07 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 
-int main() {
+inline void Flush() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+}
+
+int main(int argc, char* argv[]) {
+  Flush();
 
   do {
     std::cout << "$ ";
@@ -18,10 +22,20 @@ int main() {
       break;
     }
 
+    if (command == "echo") {
+      std::string remainingLine;
+
+      if (std::getline(std::cin,remainingLine)) {
+        std::cout << remainingLine << std::endl;
+      }
+
+      Flush();
+      continue;
+    }
+
     std::cerr << command << ": command not found" << std::endl;
     // Flush after every std::cout / std:cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+    Flush();
   } while (true);
   return 0;
 }
