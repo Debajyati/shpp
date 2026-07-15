632b3dc29fa5522b42a049e312c77c35a4f54d87
Debajyati Dey <ddebajyati@gmail.com>
2026-07-15 10:09:31 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
+#include <cstdlib>
+#include <filesystem>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 inline void Flush() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+}
+
+bool fileExists(const std::string &path) {
+  return std::filesystem::exists(path);
 }
 
 int main(int argc, char *argv[]) {
@@ -26,12 +33,48 @@ int main(int argc, char *argv[]) {
     if (command == "type") {
       std::string argument;
 
+      const char *path_val = std::getenv("PATH");
       if (std::cin >> argument) {
         if (argument == "echo" || argument == "type" || argument == "exit") {
           std::cout << argument << " is a shell builtin" << std::endl;
           Flush();
+        } else if (path_val != nullptr) {
+          std::string path_str(path_val);
+          std::stringstream ps(path_str);
+
+          std::vector<std::string> pathsArray;
+          std::string token;
+
+          while (std::getline(ps, token, ':')) {
+            pathsArray.push_back(token);
+          }
+
+          bool notfound = true;
+          for (int i = 0; i < pathsArray.size(); i++) {
+            const std::string filepath = pathsArray[i] + "/" + argument;
+            if (fileExists(filepath)) {
+              std::filesystem::perms active_permissions =
+                  std::filesystem::status(filepath).permissions();
+
+              if ((active_permissions & std::filesystem::perms::owner_exec) !=
+                      std::filesystem::perms::none ||
+                  (active_permissions & std::filesystem::perms::group_exec) !=
+                      std::filesystem::perms::none ||
+                  (active_permissions & std::filesystem::perms::others_exec) !=
+                      std::filesystem::perms::none) {
+
+                std::cout << argument << " is " << filepath << std::endl;
+                notfound = false;
+                Flush();
+                break;
+              } else
+                continue;
+            }
+          }
+          if (notfound)
+            std::cerr << argument << ": not found" << std::endl;
         } else {
-          std::cout << argument << ": not found" << std::endl;
+          std::cerr << argument << ": not found" << std::endl;
         }
       }
       continue;
